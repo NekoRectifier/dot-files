a43d65fe54a34b6e32d6617dff1d9b4cf3c894c1
NekoRectifier <hyx123250@gmail.com>
2024-06-17 15:38:42 +0800

    remove unwanted exts

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,6 +8,13 @@ set -x "__GLX_VENDOR_LIBRARY_NAME" nvidia
 set -x "__VK_LAYER_NV_optimus" NVIDIA_only
 set -x "__NV_PRIME_RENDER_OFFLOAD_PROVIDER" NVIDIA-G0
 
-set -x "GTK_IM_MODULE" xim
-set -x "QT_IM_MODULE" xim
-set -x "XMODIFERS" @im=ibus
+set -x "GTK_IM_MODULE" fcitx
+set -x "QT_IM_MODULE" fcitx
+set -x "XMODIFERS" @im=fcitx
+
+abbr -a update sudo dnf update
+abbr -a distro_s sudo dnf distro-sync
+
+
+# start tmux (in the situation of alacritty)
+tmux
